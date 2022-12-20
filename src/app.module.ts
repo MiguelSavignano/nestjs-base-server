4897ef9bc31f419e4a8055f4610243244f115f73
Miguel Savignano <m.savignano@stuart.com>
2022-12-20 03:15:52 +0100

    open telemetry default

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrometheusModule } from 'nestjs-prometheus-setup';
+import { OpenTelemetryModule } from 'nestjs-opentelemetry-setup';
 
 @Module({
-  imports: [PrometheusModule.fromRoot()],
+  imports: [
+    PrometheusModule.fromRoot(),
+    OpenTelemetryModule.fromRoot({
+      serviceName: 'base-server',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
