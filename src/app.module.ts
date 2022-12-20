47e71218f3b3094919f6b2afbb919c1b8f6c32f6
Miguel Savignano <m.savignano@stuart.com>
2022-12-20 02:44:51 +0100

    install prometheus module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrometheusModule } from 'nestjs-prometheus-setup';
 
 @Module({
-  imports: [],
+  imports: [PrometheusModule.fromRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
