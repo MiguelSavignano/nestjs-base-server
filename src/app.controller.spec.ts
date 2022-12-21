f839d206313194c0a603165f61651938b3e93dd7
Miguel Savignano <migue.masx@gmail.com>
2022-12-22 00:30:06 +0100

    test controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { OpenTelemetrySetupModule } from 'nestjs-opentelemetry-setup';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { httpClientProvider } from './http-client/axios';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        OpenTelemetrySetupModule.forRoot({
+          serviceName: 'test-app',
+        }),
+      ],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService, httpClientProvider],
     }).compile();
 
     appController = app.get<AppController>(AppController);
