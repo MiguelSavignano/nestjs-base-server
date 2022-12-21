bedda1e0da781285f44c4e96fe1cb93b886f10aa
Miguel Savignano <migue.masx@gmail.com>
2022-12-22 00:36:50 +0100

    Revert "unit test"
    This reverts commit 4faec3b9104f79af8a130f6af82fe581d7e8a8cc.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,25 @@
-import axios from 'axios';
-import { TraceService } from 'nestjs-opentelemetry-setup';
+import { Test, TestingModule } from '@nestjs/testing';
+import { OpenTelemetrySetupModule } from 'nestjs-opentelemetry-setup';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { httpClientProvider } from './http-client/axios';
 
 describe('AppController', () => {
-  const appController = new AppController(
-    new AppService(),
-    new TraceService(),
-    axios,
-  );
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        OpenTelemetrySetupModule.forRoot({
+          serviceName: 'test-app',
+        }),
+      ],
+      controllers: [AppController],
+      providers: [AppService, httpClientProvider],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
