f647512f195f5ff04d261fed4a33d901a5507abc
Miguel Savignano <migue.masx@gmail.com>
2022-12-22 00:54:02 +0100

    .

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -25,11 +25,11 @@ describe('AppController', () => {
       .reply(200, expectedTodo);
   });
 
-  // describe('root', () => {
-  //   it('should return "Hello World!"', () => {
-  //     expect(appController.getHello()).toBe('Hello World!');
-  //   });
-  // });
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
 
   describe('myEndpoint', () => {
     it('return todo 1', async () => {
