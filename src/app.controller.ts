32422ce4a3fbea476ddaec12fc8fa95e63a4b330
Miguel Savignano <m.savignano@stuart.com>
2023-03-19 22:56:41 +0100

    trigger test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { TraceService } from 'nestjs-opentelemetry-setup';
 import { JwtDecode } from 'nestjs-jwt-utils';
 import { HealthResponse } from './dto/health-response.dto';
 
+// change
 class MyCustomError extends Error {}
 
 @Controller()
